1a918cd9ca57bfdf498f08c216d0f5c5355be3ce
SimonHic <simonhicdev@gmail.com>
2025-02-04 13:03:17 +0000

    Add allOlder function to increment all friend ages.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -7,3 +7,14 @@ function older(f: Friend) : string {
 }
 
 console.log(older(friends[0]))
+
+function allOlder(f1: Friend[]) : string[] {
+    const result: string[] = [];
+    f1.forEach(f => {
+        f.age += 1;
+        result.push(`${f.name} is now ${f.age}`);
+    });
+    return result; 
+}
+
+console.log(allOlder(friends));
