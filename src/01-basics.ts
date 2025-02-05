92fe10b1278018d5c83bd7f3f164414b2c0f981b
SimonHic <simonhicdev@gmail.com>
2025-02-05 01:35:02 +0000

    Demo optional properties and parameters.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -4,12 +4,14 @@ const friend1: Friend = {
     name: "Bob Ross",
     phone: "087-12345",
     age: 25,
+    dob: new Date("1998-11-20") ,
   };
   
   const friend2: Friend = {
     name: "Ross Bob",
     phone: "086--12345",
     age: 31,
+    interests: ['Music', 'Sport']
   };
   
   export const friends: Friend[] = [friend1, friend2];
